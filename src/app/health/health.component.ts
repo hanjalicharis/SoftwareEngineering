8e4b9d3364d7ba33cf1dc5257fdd7b2833e81593
hanjalicharis <thehhare@gmail.com>
2021-05-31 12:57:09 +0200

    added corona API

--- src/app/health/health.component.ts
+++ src/app/health/health.component.ts
@@ -11,10 +11,15 @@ export class HealthComponent implements OnInit {
   constructor(private _services: NewsapiserviceService) { }
 
   healthDisplay: any = [];
+  coronaDisplay: any = [];
 
   ngOnInit(): void {
     this._services.health().subscribe((result) => {
       this.healthDisplay = result.articles;
+    })
+
+    this._services.corona().subscribe((result) => {
+      this.coronaDisplay = result.articles;
     })
   }
 
--- src/app/service/newsapiservice.service.ts
+++ src/app/service/newsapiservice.service.ts
@@ -14,7 +14,10 @@ export class NewsapiserviceService {
 
   newsApiUrl = "https://gnews.io/api/v4/search?q=example&lang=en&max=100&country=us&token=be5c3549923ecfd08cb7b7964ee3db8b";
   entertainmentApiUrl = "https://gnews.io/api/v4/search?q=entertainment&max=100&lang=en&country=us&topic=entertainment&token=be5c3549923ecfd08cb7b7964ee3db8b";
+  coronaVirusAPI = "https://api.covid19api.com/summary";
   healthApiUrl = "https://gnews.io/api/v4/search?q=health&lang=en&country=us&max=100&topic=entertainment&token=be5c3549923ecfd08cb7b7964ee3db8b";
+
+
 
   // heading 
 
@@ -28,6 +31,10 @@ export class NewsapiserviceService {
 
   health(): Observable<any> {
     return this._http.get(this.healthApiUrl);
+  }
+
+  corona(): Observable<any> {
+    return this._http.get(this.coronaVirusAPI);
   }
 
 
