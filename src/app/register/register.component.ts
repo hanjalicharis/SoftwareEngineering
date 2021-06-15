3eebc0f5f37277a141570b14b82df0a9277be6df
hanjalicharis <thehhare@gmail.com>
2021-06-15 22:51:15 +0200

    added components

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  URL: string = "https://se-backend-hanjalic.herokuapp.com/api/users/register/";
+
   constructor() { }
 
   ngOnInit(): void {
